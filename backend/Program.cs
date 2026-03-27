25f6f4f3e4ed58518a4e06bf6722f0a98086e0bd
marloww10 <marlon.franco@gmail.com>
2026-03-26 23:51:57 -0300

    alteracoes para commit 2

--- backend/Program.cs
+++ backend/Program.cs
@@ -93,6 +93,10 @@ builder.Services.AddDbContext<AppDbContext>(option =>
 
             var app = builder.Build();
 
+            using (var scope = app.Services.CreateScope()) {
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
